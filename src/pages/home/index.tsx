f52714933f320ee2451f0ebe119c8266b001d5a8
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-08-17 16:52:01 -0300

    Exercício 2 feito

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <div>
+      <input type="text" />
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+    </div>
+  );
+}
