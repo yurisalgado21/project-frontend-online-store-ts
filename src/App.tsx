0a9ca2f3d6dd8b95bfd05ecb3c8241b84fc08f92
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-08-17 18:01:00 -0300

    Exercício 3 feito

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
+import ShoppingCartPage from './pages/ShoppingCartPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> } />
+      <Route path="/carrinho" element={ <ShoppingCartPage /> } />
     </Routes>
   );
 }
