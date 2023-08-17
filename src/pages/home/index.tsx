77941c6b9b0df71d334770eabc88d3178af13439
Claus Haas <claus.haas@me.com>
2023-08-17 17:35:34 -0300

    Exercício 4 feito

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { getCategories } from '../../services/api';
 
-export default function index() {
+type Categorie = {
+  id: string,
+  name: string,
+};
+
+export default function Home() {
+  const [categories, setCategories] = useState<Categorie[]>();
+
+  useEffect(() => {
+    const getCategoriesData = async () => {
+      const data = await getCategories();
+      setCategories(data);
+    };
+
+    getCategoriesData();
+  }, []);
   return (
-    <div>
-      <input type="text" />
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
-    </div>
+    <>
+      <aside>
+        <p>Categorias:</p>
+        {categories && categories.map((category) => (
+          <div key={ category.id }>
+            <label htmlFor={ category.id } data-testid="category">
+              <input type="radio" name="category" id={ category.id } />
+              {category.name}
+            </label>
+          </div>
+        ))}
+      </aside>
+      <main>
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </main>
+    </>
   );
 }
